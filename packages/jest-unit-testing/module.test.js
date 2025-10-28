529b95a815e69c30c7f476562c728f39ea11eff4
vic <vgrigory@calpoly.edu>
2025-10-28 13:07:05 -0700

    added names to the tests

--- packages/jest-unit-testing/module.test.js
+++ packages/jest-unit-testing/module.test.js
@@ -8,39 +8,39 @@ test('Sum test: completed successfully', () => {
 });
 
 //div
-test('Div test: completed successfully', () => {
+test('Div test: simple', () => {
     const expected = 5;
     const got = mut.div(20, 4);
     expect(got).toBe(expected);
 });
-test('', () => {
+test('Div test: remainder', () => {
     const expected = 3.25;
     const got = mut.div(13, 4);
     expect(got).toBe(expected);
 });
-test('', () => {
+test('Div test: divide by zero', () => {
     const got = mut.div(1, 0);
     expect(got).toBe(Infinity);
 });
 
 //containsNumbers
-test('', () => {
+test('ContainsNumbers: one number', () => {
     const got = mut.containsNumbers('4');
     expect(got).toBeTruthy();
 });
-test('', () => {
+test('ContainsNumbers: one letter', () => {
     const got = mut.containsNumbers('e');
     expect(got).toBeFalsy();
 });
-test('Contains numbers test: completed successfully', () => {
+test('ContainsNumbers: many numbers and letters', () => {
     const got = mut.containsNumbers('fbwf7efrugbwe90');
     expect(got).toBeTruthy();
 });
-test('', () => {
+test('ContainsNumbers: many letters', () => {
     const got = mut.containsNumbers('fbrewvjwbvewinvweiabvkbwal');
     expect(got).toBeFalsy();
 });
-test('', () => {
+test('ContainsNumbers: empty', () => {
     const got = mut.containsNumbers('');
     expect(got).toBeFalsy();
 });
